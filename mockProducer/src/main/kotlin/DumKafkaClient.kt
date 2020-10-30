1ffae150d42d153487e1b225a2fe0fd72ceaf077
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-10-30 11:17:26 +0100

    mer kafka config

--- mockProducer/src/main/kotlin/DumKafkaClient.kt
+++ mockProducer/src/main/kotlin/DumKafkaClient.kt
@@ -9,7 +9,11 @@ private fun createProducer(): Producer<String, String> {
     props["bootstrap.servers"] = System.getenv("KAFKA_BROKERS")
     props["key.serializer"] = StringSerializer::class.java.canonicalName
     props["value.serializer"] = StringSerializer::class.java.canonicalName
-
+    props["ssl.keystore.location"] = System.getenv("KAFKA_KEYSTORE_PATH")
+    props["ssl.keystore.password"] = System.getenv("KAFKA_CREDSTORE_PASSWORD")
+    props["ssl.truststore.location"] = System.getenv("KAFKA_TRUSTSTORE_PATH")
+    props["ssl.truststore.password"] = System.getenv("KAFKA_CREDSTORE_PASSWORD")
+    props["security.protocol"] = System.getenv("SSL")
     return KafkaProducer<String, String>(props)
 }
 
