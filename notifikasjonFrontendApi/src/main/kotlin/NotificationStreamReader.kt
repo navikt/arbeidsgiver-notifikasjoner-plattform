083f1a629cdd744c8d9315100c166bd6cef8b69c
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-11-09 13:31:10 +0100

    konfigurert notifikasjonsstreamreader

--- notifikasjonFrontendApi/src/main/kotlin/NotificationStreamReader.kt
+++ notifikasjonFrontendApi/src/main/kotlin/NotificationStreamReader.kt
@@ -1,17 +1,34 @@
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import no.nav.arbeidsgiver.Notifikasjon
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.serialization.StringDeserializer
+import java.net.URI
+import java.net.URL
 import java.util.*
 
-fun lagKafkaConsumer(): KafkaConsumer<String, String> {
+fun lagKafkaConsumer(): KafkaConsumer<String, Notifikasjon> {
     val props = Properties()
     props["bootstrap.servers"] = System.getenv("KAFKA_BROKERS") ?: ""
-    props["key.serializer"] = StringSerializer::class.java.canonicalName
-    props["value.serializer"] = StringSerializer::class.java.canonicalName
+    props["key.deserializer"] = StringDeserializer::class.java.canonicalName
+    props["value.deserializer"] = KafkaAvroDeserializer::class.java.canonicalName
     props["ssl.keystore.location"] = System.getenv("KAFKA_KEYSTORE_PATH") ?: ""
     props["ssl.keystore.password"] = System.getenv("KAFKA_CREDSTORE_PASSWORD") ?: ""
     props["ssl.truststore.location"] = System.getenv("KAFKA_TRUSTSTORE_PATH") ?: ""
     props["ssl.truststore.password"] = System.getenv("KAFKA_CREDSTORE_PASSWORD") ?: ""
     props["security.protocol"] = "SSL"
+    val schemaRegistry = URL(System.getenv("KAFKA_SCHEMA_REGISTRY"))
+    props["basic.auth.credentials.source"] = "USER_INFO"
+    props["basic.auth.user.info"] = schemaRegistry.userInfo
+    props["group.id"] = "notifikasjon-frontend-api"
+    props["schema.registry.url"] = URI(
+            schemaRegistry.protocol,
+            null,
+            schemaRegistry.host,
+            schemaRegistry.port,
+            schemaRegistry.path,
+            schemaRegistry.query,
+            schemaRegistry.ref
+    ).toString()
     return KafkaConsumer(props)
 }
 
