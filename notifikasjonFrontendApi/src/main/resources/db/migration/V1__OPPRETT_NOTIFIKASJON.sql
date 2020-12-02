1d37435c69f5c0c35c46036c2152316a7150e1f4
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2020-12-02 13:05:21 +0100

    sletter gammel oppretting av database (tabeller slettet manuelt)

--- notifikasjonFrontendApi/src/main/resources/db/migration/V1__OPPRETT_NOTIFIKASJON.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS notifikasjon
-(
-    id  INT PRIMARY KEY,
-    beskjed TEXT NOT NULL,
-    orgnr CHAR(9) NOT NULL,
-    servicecode VARCHAR(4) NOT NULL,
-    serviceedition INT NOT NULL
-);
