1d818bcb28cbca8eff4f5f71db9b3e8373a0a0fe
Terese Haug <terese.haug@nav.no>
2020-12-04 10:24:58 +0100

    Lagt til attributter i sql spørring

--- notifikasjonFrontendApi/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/config/DatabaseOpaerasjoner.kt
+++ notifikasjonFrontendApi/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/config/DatabaseOpaerasjoner.kt
@@ -7,7 +7,31 @@ import javax.sql.DataSource
 
 
 fun Connection.leggTilNotifikasjon (nokkel: Nokkel, notifikasjon: Notifikasjon){
-    var prepstat = this.prepareStatement("insert into notifikasjon values (id=?)")
-    prepstat.setInt(1,1)
+    val prepstat = this.prepareStatement("""
+        insert into notifikasjon values (
+            tjenestenavn=?, 
+            eventid=?, 
+            fnr=?, 
+            orgnr=?, 
+            servicecode=?, 
+            serviceedition=?, 
+            tidspunkt=?, 
+            synlig_frem_til=?, 
+            tekst=?, 
+            link=?, 
+            grupperingsid=?
+        )
+    """)
+    prepstat.setString(1, nokkel.tjenestenavn.toString())
+    prepstat.setString(2,nokkel.eventId.toString())
+    prepstat.setString(2,nokkel.fnr.toString())
+    prepstat.setString(2,nokkel.orgnr.toString())
+    prepstat.setString(2,nokkel.servicecode.toString())
+    prepstat.setString(2,nokkel.serviceedition.toString())
+    prepstat.setString(2,nokkel.tidspunkt.toString())
+    prepstat.setString(2,nokkel.synlig_frem_til.toString())
+    prepstat.setString(2,nokkel.tekst.toString())
+    prepstat.setString(2,nokkel.link.toString())
+    prepstat.setString(2,nokkel.grupperingsid.toString())
 
 }
