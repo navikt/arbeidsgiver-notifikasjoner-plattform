bfb9ad8766257c1f15b8572acffeebfcbfbf1676
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-12-04 11:02:44 +0100

    fortsettelse sett inn verdier i databse table

--- notifikasjonFrontendApi/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/config/DatabaseOpaerasjoner.kt
+++ notifikasjonFrontendApi/src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/config/DatabaseOpaerasjoner.kt
@@ -1,12 +1,11 @@
 package no.nav.arbeidsgiver.notifikasjon.config
 
-import no.nav.arbeidsgiver.notifikasjon.Nokkel
-import no.nav.arbeidsgiver.notifikasjon.Notifikasjon
+import no.nav.arbeidsgiver.notifikasjon.*
 import java.sql.Connection
 import javax.sql.DataSource
 
 
-fun Connection.leggTilNotifikasjon (nokkel: Nokkel, notifikasjon: Notifikasjon){
+fun Connection.leggTilNotifikasjon (nokkel: Nokkel, notifikasjonWrapper: Notifikasjon){
     val prepstat = this.prepareStatement("""
         insert into notifikasjon values (
             tjenestenavn=?, 
@@ -24,14 +23,27 @@ fun Connection.leggTilNotifikasjon (nokkel: Nokkel, notifikasjon: Notifikasjon){
     """)
     prepstat.setString(1, nokkel.tjenestenavn.toString())
     prepstat.setString(2,nokkel.eventId.toString())
-    prepstat.setString(2,nokkel.fnr.toString())
-    prepstat.setString(2,nokkel.orgnr.toString())
-    prepstat.setString(2,nokkel.servicecode.toString())
-    prepstat.setString(2,nokkel.serviceedition.toString())
-    prepstat.setString(2,nokkel.tidspunkt.toString())
-    prepstat.setString(2,nokkel.synlig_frem_til.toString())
-    prepstat.setString(2,nokkel.tekst.toString())
-    prepstat.setString(2,nokkel.link.toString())
-    prepstat.setString(2,nokkel.grupperingsid.toString())
-
+    val notifikasjon = notifikasjonWrapper.notifikasjon
+    val mottaker = notifikasjonWrapper.mottaker.mottaker
+    when (mottaker) {
+        is OrgnrFnr -> {
+            prepstat.setString(3, mottaker.fnr.toString());
+            prepstat.setString(4, mottaker.orgnr.toString())
+        }
+        is OrgnrAltinnservice -> {
+            prepstat.setString(4, mottaker.orgnr.toString())
+            prepstat.setString(5, mottaker.servicecode.toString())
+            prepstat.setString(6, mottaker.serviceedition.toString())
+        }
+    }
+    prepstat.setLong(7, notifikasjonWrapper.tidspunkt);
+    when (notifikasjon) {
+        is Beskjed -> {
+            prepstat.setLong(8, notifikasjon.synligFremTil);
+            prepstat.setString(9, notifikasjon.tekst.toString());
+            prepstat.setString(10, notifikasjon.link.toString());
+        }
+    }
+    prepstat.setString(11, notifikasjonWrapper.grupperingsId.toString());
+    prepstat.execute();
 }
